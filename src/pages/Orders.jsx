bbc09350a19343a4f479c861e0469ff2b09258b7
ShowGa <showgacareer@gmail.com>
2024-12-02 11:25:22 +0800

    <Modify> 1. Modify the text

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -10,7 +10,7 @@ const Orders = () => {
 
         <div className="flex flex-col gap-4 mt-10 border border-gray-200 rounded-lg">
           <h2 className="border-b border-gray-200 px-6 pt-6 pb-4 text-2xl font-semibold">
-            你的商品
+            顧客訂單資訊
           </h2>
 
           <div className="flex flex-wrap gap-4 pb-6 px-6">
--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -10,7 +10,7 @@ const Store = () => {
 
         <div className="flex flex-col gap-4 mt-10 border border-gray-200 rounded-lg">
           <h2 className="border-b border-gray-200 px-6 pt-6 pb-4 text-2xl font-semibold">
-            你的商品
+            店鋪販售的商品
           </h2>
 
           <div className="pb-6 px-6">
