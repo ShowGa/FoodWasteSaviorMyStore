df7b7d7bc40ba2e47b92b43910ad740726de6498
ShowGa <showgacareer@gmail.com>
2024-12-13 11:52:40 +0800

    <Progress> 1. Progress : Create Step3.jsx logic functionality

--- src/components/register-steps/Step3.jsx
+++ src/components/register-steps/Step3.jsx
@@ -1,22 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 // zustand
 import useRegFormStore from "../../zustand/useRegFormStore";
+// react map gl
+import Map, { Marker } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+// react icons
+import { RiMapPin2Fill } from "react-icons/ri";
+import { FaShoppingBag } from "react-icons/fa";
+// css
+import "../css/component.css";
 
 const Step3 = () => {
-  const { formData } = useRegFormStore();
+  const { formData, setFormData } = useRegFormStore();
+  const [viewState, setViewState] = useState({
+    longitude: formData.longitude ? formData.longitude : 121.5654,
+    latitude: formData.latitude ? formData.latitude : 25.033,
+    zoom: 14,
+  });
+  // onMoving state
+  const [onMoving, setOnMoving] = useState(false);
 
   // ============= helper function ============= //
-  const handleTestForm = () => {
-    console.log(formData);
-  };
 
   return (
     <>
       <h2 className="text-3xl">請查看您的商店標記是否正確</h2>
       <span className="text-md text-gray-400">幫助我們更快的找到您的愛店</span>
 
-      <div className="w-full h-[25rem] rounded-2xl mt-10 bg-gray-200">
-        <button onClick={handleTestForm}>test form</button>
+      <div className="w-full h-[25rem] rounded-2xl mt-10 bg-gray-200 relative">
+        <Map
+          {...viewState}
+          onMove={(evt) => {
+            setViewState(evt.viewState);
+            setOnMoving(true);
+            setFormData({
+              ...formData,
+              longitude: evt.viewState.longitude,
+              latitude: evt.viewState.latitude,
+            });
+          }}
+          onMoveEnd={() => {
+            setOnMoving(false);
+          }}
+          style={{ width: "100%", height: "100%" }}
+          mapStyle="mapbox://styles/mapbox/streets-v11"
+          mapboxAccessToken={import.meta.env.VITE_MAPBOX_API_KEY}
+        ></Map>
+
+        <div className="map-pin-icon-container">
+          <div className={`map-pin-icon ${onMoving ? "icon-drag-effect" : ""}`}>
+            <RiMapPin2Fill className="text-4xl text-red-500" />
+          </div>
+        </div>
       </div>
     </>
   );
